b4a00be1a93828833dd75b030c0dfa64981376ba
langnang <langnang.chen@outlook.com>
2022-06-22 21:12:07 +0800

    build

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+const gulp = require('gulp')
+  , sass = require('gulp-sass')(require('sass'))
+  , minifyCSS = require('gulp-minify-css')
+  , autoprefixer = require('gulp-autoprefixer')
+  , clean = require('gulp-clean')
+  , rename = require("gulp-rename")
+  ;
+
+gulp.task('clean', () => gulp.src('./dist').pipe(clean()))
+
+gulp.task('sass', () => gulp.src('./packages/index.scss')
+  .pipe(sass())
+  .pipe(autoprefixer({
+    browsers: ["last 4 versions", "Firefox >= 27", "Blackberry >= 7", "IE 8", "IE 9"],
+    cascade: false
+  }))
+  .pipe(rename({
+    basename: 'ln'
+  }))
+  .pipe(gulp.dest('dist'))
+)
+
+gulp.task('min', () => gulp.src('./dist/**/*.css')
+  .pipe(minifyCSS())
+  .pipe(rename({
+    suffix: ".min",
+  }))
+  .pipe(gulp.dest('dist'))
+)
+
+gulp.task('watch', () => {
+  gulp.watch('./packages/**', gulp.series(['sass']))
+})
+
+gulp.task('default', gulp.series(['clean', 'sass', 'min']))
